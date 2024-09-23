9be8a4d271802e101d3db1f9f4c503935874f6b9
Rocio Mexia <r.mexiadiaz@hotmail.com>
2024-09-23 11:27:40 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,17 @@
-tissue = cellsim.Tissue(10,40)
-tissue.seed_random(0.5,cellsim.Cell)
+% Parameters
+rows = 10
+columns = 40
+probability_alive = 0.5
+
+% Instantiate the Tissue
+tissue = cellsim.Tissue(rows,columns)
+tissue.seed_random(probability_alive,cellsim.Cell)
 print('time step:', 0)
 print(tissue)
+
+% For 100 time steps update the tissue
 for i in range(1,100):
-    os.system('clear') #will be os.system('cls')
+    os.system('cls') 
     print('time step:', i) 
     tissue.next_state()
     print(tissue)
