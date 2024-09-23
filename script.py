6a4f799f71573ef3aa5846521eab786982f5ba80
Rocio Mexia <r.mexiadiaz@hotmail.com>
2024-09-23 11:28:16 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
-% Parameters
+# Parameters
 rows = 10
 columns = 40
 probability_alive = 0.5
 
-% Instantiate the Tissue
+# Instantiate the Tissue
 tissue = cellsim.Tissue(rows,columns)
 tissue.seed_random(probability_alive,cellsim.Cell)
 print('time step:', 0)
 print(tissue)
 
-% For 100 time steps update the tissue
+# For 100 time steps update the tissue
 for i in range(1,100):
     os.system('cls') 
     print('time step:', i) 
